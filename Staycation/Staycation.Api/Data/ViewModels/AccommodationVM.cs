ae93aa784006afdbd2b75308ccdb5c2ed3e97e81
Tomislav Periša <tomislav.pingi.perisa@gmail.com>
2022-08-12 14:08:32 +0200

    Feat: [Z-2] Add DTO model validation for accommodationVM

--- Staycation/Staycation.Api/Data/ViewModels/AccommodationVM.cs
+++ Staycation/Staycation.Api/Data/ViewModels/AccommodationVM.cs
@@ -1,19 +1,41 @@
 ﻿using Staycation.Api.Data.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Staycation.Api.Data.ViewModels
 {
     public class AccommodationVM
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
-        public string Subtitle { get; set; }
-        public string Description { get; set; }
+
+        [MaxLength(100)]
+        public string? Subtitle { get; set; }
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+        [MaxLength(50)]
+        [Required]
         public string Type { get; set; }
+
+        [Required]
         public int Categorization { get; set; }
+
+        [Required]
         public int PersonCount { get; set; }
-        public string ImageUrl { get; set; }
+
+        [MaxLength(800)]
+        public string? ImageUrl { get; set; }
         public bool? FreeCancelation { get; set; }
+
+        [Required]
         public decimal Price { get; set; }
+
+        [Required]
         public string LocationName { get; set; }
+
+        [Required]
         public string PostalCode { get; set; }
     }
 }
