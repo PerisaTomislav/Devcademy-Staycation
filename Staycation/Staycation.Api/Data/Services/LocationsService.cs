769a3c31c0176f0cce03235f3be43dc72e12ee51
Tomislav Periša <tomislav.pingi.perisa@gmail.com>
2022-08-12 14:55:51 +0200

    Feat: [Z-3] Add read, update and delete location service methods

--- Staycation/Staycation.Api/Data/Services/LocationsService.cs
+++ Staycation/Staycation.Api/Data/Services/LocationsService.cs
@@ -17,12 +17,43 @@ namespace Staycation.Api.Data.Services
         {
             var _location = new Location()
             {
+                ImageUrl = locationVM.ImageUrl,
                 Name = locationVM.Name,
                 PostalCode = locationVM.PostalCode
             };
             _context.Locations.Add(_location);
             _context.SaveChanges();
             return _location;
+        }
+
+        public List<Location> GetLocations()
+        {
+            var allLocations = _context.Locations.ToList();
+            return allLocations;
+        }
+
+        public Location UpdateLocationById(int id, LocationVM locationVM)
+        {
+            var _location = _context.Locations.FirstOrDefault(n => n.Id == id);
+            if (_location != null)
+            {
+                _location.ImageUrl = locationVM.ImageUrl;
+                _location.Name = locationVM.Name;
+                _location.PostalCode = locationVM.PostalCode;
+
+                _context.SaveChanges();
+            }
+            return _location;
+        }
+
+        public void DeleteLocationById(int id)
+        {
+            var _location = _context.Locations.FirstOrDefault(n => n.Id == id);
+            if (_location != null)
+            {
+                _context.Remove(_location);
+                _context.SaveChanges();
+            }
         }
     }
 }
