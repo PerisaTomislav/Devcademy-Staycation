28a552decbe42395f5ecec6272268d8794e6344f
Tomislav Periša <tomislav.pingi.perisa@gmail.com>
2022-08-12 15:01:22 +0200

    Feat: [Z-3] Add reservationVM

---
+++ Staycation/Staycation.Api/Data/ViewModels/ReservationVM.cs
@@ -0,0 +1,11 @@
+﻿namespace Staycation.Api.Data.ViewModels
+{
+    public class ReservationVM
+    {
+        public string Email { get; set; }
+        public DateTime? CheckIn { get; set; }
+        public DateTime? CheckOut { get; set; }
+        public int PersonCount { get; set; }
+        public bool Confirmed { get; set; }
+    }
+}
